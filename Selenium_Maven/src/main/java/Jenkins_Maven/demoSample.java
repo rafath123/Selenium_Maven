9465e18414f75711f64fca5d273306a278778b3d
Admin <Admin@DESKTOP-DVQBT9H>
2023-08-02 18:24:41 +0100

    02Aug2023 first commit.

---
+++ Selenium_Maven/src/main/java/Jenkins_Maven/demoSample.java
@@ -0,0 +1,10 @@
+package Jenkins_Maven;
+
+public class demoSample {
+	
+	public String sample() {
+		System.out.println("Hii GitHub");
+		return "Sample" ;
+	}
+       
+}
---
+++ Selenium_Maven/src/test/java/Jenkins_Maven/sampleTest.java
@@ -0,0 +1,14 @@
+package Jenkins_Maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
